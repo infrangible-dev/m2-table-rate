d53299d04b560a312a9f5d5bf71ee574b874e4bd
Andreas Knollmann <a.knollmann@seak.dev>
2024-11-26 07:50:58 +0100

    update backend widget module

--- src/Block/Adminhtml/TableRate/Grid.php
+++ src/Block/Adminhtml/TableRate/Grid.php
@@ -12,26 +12,47 @@ use Magento\Framework\Data\Collection\AbstractDb;
  * @copyright   2014-2024 Softwareentwicklung Andreas Knollmann
  * @license     http://www.opensource.org/licenses/mit-license.php MIT
  */
-class Grid
-    extends \Infrangible\BackendWidget\Block\Grid
+class Grid extends \Infrangible\BackendWidget\Block\Grid
 {
-    protected function prepareCollection(AbstractDb $collection)
+    protected function prepareCollection(AbstractDb $collection): void
     {
     }
 
     /**
      * @throws \Exception
      */
-    protected function prepareFields()
+    protected function prepareFields(): void
     {
         $this->addWebsiteNameColumn('website_id');
-        $this->addCountryColumn('dest_country_id', __('Country')->render());
-        $this->addRegionAnyColumn('dest_region_id', __('Region')->render());
-        $this->addTextColumn('dest_zip', __('Postcode')->render());
-        $this->addOptionsClassColumn('condition_name', __('Condition')->render(), TableRate::class);
-        $this->addNumberColumn('condition_value', __('Value')->render());
-        $this->addNumberColumn('price', __('Price')->render());
-        $this->addNumberColumn('cost', __('Cost')->render());
+        $this->addCountryColumn(
+            'dest_country_id',
+            __('Country')->render()
+        );
+        $this->addRegionAnyColumn(
+            'dest_region_id',
+            __('Region')->render()
+        );
+        $this->addTextColumn(
+            'dest_zip',
+            __('Postcode')->render()
+        );
+        $this->addOptionsClassColumn(
+            'condition_name',
+            __('Condition')->render(),
+            TableRate::class
+        );
+        $this->addNumberColumn(
+            'condition_value',
+            __('Value')->render()
+        );
+        $this->addNumberColumn(
+            'price',
+            __('Price')->render()
+        );
+        $this->addNumberColumn(
+            'cost',
+            __('Cost')->render()
+        );
     }
 
     protected function getHiddenFieldNames(): array
